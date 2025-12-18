f8990cbb0d54c0f3e39cd304fbf1ff09d0795561
bala-amypo <bala@amypo.in>
2025-12-18 06:17:24 +0000

    User commit

--- src/main/java/com/example/demo/entity/Location.java
+++ src/main/java/com/example/demo/entity/Location.java
@@ -1,9 +1,14 @@
-public class ComplianceLog{
+import java.time.LocalDateTime;
+public class Location{
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    @Column()
+    @Column(unique=true)
     private String locationName;
-    private String sensorType;
-    private 
+    private String description;
+    private String region;
+    private LocalDateTime installedAt;
+    private Boolean isActive;
+
+    public Location(Long id,String locationName,String description,String region,)
 }
