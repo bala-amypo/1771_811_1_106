1558642a51be1c32c1818582c0f8a6e0ce735272
bala-amypo <bala@amypo.in>
2025-12-18 06:07:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/ComplianceLog.java
@@ -0,0 +1,8 @@
+public class ComplianceLog{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String sensorCode;
+    private String sensorType;
+    private 
+}
