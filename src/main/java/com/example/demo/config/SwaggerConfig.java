34ba82808d109bb893e76bbbe22208f591b9bffe
bala-amypo <bala@amypo.in>
2025-12-20 18:19:42 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,23 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Primary
-    @Bean   
-    public OpenAPI swaggerOpenAPI() {
+    @Bean
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .info(new Info()
+                        .title("Water Quality Compliance API")
+                        .version("1.0")
+                        .description("API for monitoring and evaluating water quality compliance"))
                 .servers(List.of(
                         new Server().url("https://9215.408procr.amypo.ai/")
                 ));
