6138595df0cf742c7c7713d2f4181f340b5933d3
bala-amypo <bala@amypo.in>
2025-12-25 16:28:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/ComplianceCheckResult.java
+++ src/main/java/com/example/demo/entity/ComplianceCheckResult.java
@@ -5,11 +5,23 @@ public class ComplianceCheckResult {
     private boolean compliant;
     private String message;
 
+    private double readingValue;
+    private double minThreshold;
+    private double maxThreshold;
+
+    private String sensorType;
+
     public ComplianceCheckResult() {}
 
-    public ComplianceCheckResult(boolean compliant, String message) {
+    public ComplianceCheckResult(boolean compliant, String message,
+                                 double readingValue, double minThreshold,
+                                 double maxThreshold, String sensorType) {
         this.compliant = compliant;
         this.message = message;
+        this.readingValue = readingValue;
+        this.minThreshold = minThreshold;
+        this.maxThreshold = maxThreshold;
+        this.sensorType = sensorType;
     }
 
     public boolean isCompliant() {
@@ -26,5 +38,37 @@ public class ComplianceCheckResult {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public double getReadingValue() {
+        return readingValue;
+    }
+
+    public void setReadingValue(double readingValue) {
+        this.readingValue = readingValue;
+    }
+
+    public double getMinThreshold() {
+        return minThreshold;
+    }
+
+    public void setMinThreshold(double minThreshold) {
+        this.minThreshold = minThreshold;
+    }
+
+    public double getMaxThreshold() {
+        return maxThreshold;
+    }
+
+    public void setMaxThreshold(double maxThreshold) {
+        this.maxThreshold = maxThreshold;
+    }
+
+    public String getSensorType() {
+        return sensorType;
+    }
+
+    public void setSensorType(String sensorType) {
+        this.sensorType = sensorType;
     }
 }
