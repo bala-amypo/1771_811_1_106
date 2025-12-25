8f07101df51724960c25cc22dcfd205cacdc5e14
bala-amypo <bala@amypo.in>
2025-12-25 15:19:26 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorReadingRepository.java
+++ src/main/java/com/example/demo/repository/SensorReadingRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
