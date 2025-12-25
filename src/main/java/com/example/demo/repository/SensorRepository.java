fd2a68773bccaa22709bb9b9c83219312626b9d8
bala-amypo <bala@amypo.in>
2025-12-25 16:22:25 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorRepository.java
+++ src/main/java/com/example/demo/repository/SensorRepository.java
@@ -4,4 +4,7 @@ import com.example.demo.entity.Sensor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SensorRepository extends JpaRepository<Sensor, Long> {
+
+    // test cases require: find sensor by type
+    Sensor findBySensorType(String sensorType);
 }
