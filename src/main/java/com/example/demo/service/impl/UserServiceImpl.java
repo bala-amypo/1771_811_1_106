fbd6463d147ac21cd144ba1494676058ac9d86c5
bala-amypo <bala@amypo.in>
2025-12-25 17:04:26 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,11 +1,12 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.entity.User;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,22 +22,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(User user) {
-        if (user.getRole() == null) {
-            user.setRole("USER");
-        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     @Override
     public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
-    }
+        Optional<User> userOpt = userRepository.findByEmail(email);
 
-    @Override
-    public User findById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        return userOpt.orElseThrow(() ->
+                new RuntimeException("User not found"));
     }
 }
