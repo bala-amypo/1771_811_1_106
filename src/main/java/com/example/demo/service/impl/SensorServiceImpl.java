2024c0b3870dc51a7ebb2e3005ebdbe0c509eabb
bala-amypo <bala@amypo.in>
2025-12-26 06:33:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/SensorServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Location;
+import com.example.demo.entity.Sensor;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.repository.LocationRepository;
+import com.example.demo.repository.SensorRepository;
+import com.example.demo.service.SensorService;
+
+import java.util.List;
+
+public class SensorServiceImpl implements SensorService {
+
+    private final SensorRepository sensorRepository;
+    private final LocationRepository locationRepository;
+
+    public SensorServiceImpl(SensorRepository sensorRepository,
+                             LocationRepository locationRepository) {
+        this.sensorRepository = sensorRepository;
+        this.locationRepository = locationRepository;
+    }
+
+    @Override
+    public Sensor createSensor(Long locationId, Sensor sensor) {
+        Location location = locationRepository.findById(locationId)
+                .orElseThrow(() -> new ResourceNotFoundException("Location not found"));
+
+        if (sensor.getSensorType() == null || sensor.getSensorType().isEmpty()) {
+            throw new IllegalArgumentException("sensorType required");
+        }
+
+        sensor.setLocation(location);
+        return sensorRepository.save(sensor);
+    }
+
+    @Override
+    public Sensor getSensor(Long id) {
+        return sensorRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Sensor not found"));
+    }
+
+    @Override
+    public List<Sensor> getAllSensors() {
+        return sensorRepository.findAll();
+    }
+}
