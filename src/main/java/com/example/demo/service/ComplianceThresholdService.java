ae0a5dfa35c64c4585cfcaea9bc310b6adfbe866
bala-amypo <bala@amypo.in>
2025-12-26 06:27:31 +0000

    User commit

--- src/main/java/com/example/demo/service/ComplianceThresholdService.java
+++ src/main/java/com/example/demo/service/ComplianceThresholdService.java
@@ -1,13 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ComplianceThreshold;
+
 import java.util.List;
 
 public interface ComplianceThresholdService {
 
-    List<ComplianceThreshold> getAllThresholds();
-
     ComplianceThreshold createThreshold(ComplianceThreshold threshold);
 
+    ComplianceThreshold getThreshold(Long id);
+
     ComplianceThreshold getThresholdBySensorType(String sensorType);
+
+    List<ComplianceThreshold> getAllThresholds();
 }
