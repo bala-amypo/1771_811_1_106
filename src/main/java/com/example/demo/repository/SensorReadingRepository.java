376f70e4b9c064d5f87294be05974380ebc01baf
bala-amypo <bala@amypo.in>
2025-12-26 12:13:14 +0000

    User commit

--- src/main/java/com/example/demo/repository/SensorReadingRepository.java
+++ src/main/java/com/example/demo/repository/SensorReadingRepository.java
@@ -2,10 +2,10 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 import java.util.Optional;
 
 public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
 
-    Optional<SensorReading> findBySensorReadingId(Long id); // matches test expectation
+    // Custom alias expected by tests
+    Optional<SensorReading> findSensorReadingById(Long id);
 }
